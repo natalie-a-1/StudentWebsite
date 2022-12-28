b0449bcfded604fe84b8b327ecf13d6302235594
nataliehill <natalie.a.hill-1@ou.edu>
2022-12-27 19:16:02 -0600

    Returns schedule based on day

--- main.py
+++ main.py
@@ -1,11 +1,35 @@
-from flask import Flask
+from flask import Flask, render_template
+import datetime
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return "hello"
+    today = datetime.datetime.today().weekday()
+    if today == (0 or 6):
+        schedule = {}
+    elif today == (1 or 3):
+        schedule = {
+            '8:00am-11:00am : work',
+            '11:30am-12:20pm : principles of programming',
+            '1:30pm-2:20pm : linear algebra',
+            '3:30pm-5:15pm : professional development',
+        }
+    elif today == (2 or 4):
+        schedule = {
+            '9:00am-10:15am : intro to operating systems',
+            '11:30am-12:20pm : work',
+            '4:30pm-5:15pm : software engineering',
+
+    }
+    elif (today == 5):
+        schedule = {
+            '8:00am-11:00am : work',
+            '11:30am-12:20pm : principles of programming',
+            '1:30pm-2:20pm : linear algebra',
+        }
+    return render_template('index.html', schedule = schedule)
 
 # @app.route("/login")
 # def login():
